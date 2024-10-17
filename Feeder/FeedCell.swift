909218814bb31dd642ddbb5dae39a597e223815a
Rob Stearn <robstearn@me.com>
2024-10-17 14:14:51 +0400

    Icons, labels, sheet and filtering now working for iOS and iPadOS

---
+++ Feeder/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  Feeder
+//
+//  Created by Rob Stearn on 17/10/2024.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
