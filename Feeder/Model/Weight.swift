067d0768fe61504c09a42ec78c6e8f7d0bc9360c
Rob Stearn <robstearn@me.com>
2024-11-09 16:40:47 +0400

    Added a type to thge weight model to distinguish a birth weight

--- Feeder/Model/Weight.swift
+++ Feeder/Model/Weight.swift
@@ -8,11 +8,17 @@
 import Foundation
 import SwiftData
 
+enum WeightType: String, Codable, CaseIterable {
+    case weight
+    case birthWeight
+}
+
 @Model
 final class Weight: Identifiable {
     var id = UUID()
     var weight: Double
     var date: Date
+    var type: WeightType = WeightType.weight
     
     init(weight: Double, date: Date) {
         self.weight = weight
---
+++ Feeder/Views/Feed Views/TotalView.swift
@@ -0,0 +1,45 @@
+//
+//  TotalView.swift
+//  Feeder
+//
+//  Created by Rob Stearn on 13/10/2024.
+//
+
+import SwiftUI
+
+struct TotalView: View {
+    @AppStorage("feedTarget") private var feedTarget: Double?
+    var qty_ml: Int = 0
+    var highlightTarget: Bool
+
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("🍼")
+                    .font(.title)
+                    .padding()
+                Text("Total:")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.gray)
+                    .padding()
+                Text("\(qty_ml) ml")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(highlightTarget ? ( qty_ml >= /*self.dailyFeedTarget*/ Int(feedTarget ?? 500) ? .green : .red) : .primary)
+                    .padding()
+            }
+            if highlightTarget == true {
+                Text("Daily target is \(Int(feedTarget ?? 500)) ml")
+                    .font(.footnote)
+                    .tint(.gray)
+                    .fontWeight(.semibold)
+            }
+        }
+    }
+}
+
+#Preview {
+    TotalView(highlightTarget: true)
+}
