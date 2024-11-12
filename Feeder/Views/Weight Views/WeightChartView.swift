b845771bdd915757f8bc67dcbd6f66dac3235b88
Rob Stearn <robstearn@me.com>
2024-11-12 13:50:12 +0400

    Started on a Charts view - IN PROGRESS

--- Feeder/Views/Weight Views/WeightChartView.swift
+++ Feeder/Views/Weight Views/WeightChartView.swift
@@ -6,10 +6,45 @@
 //
 
 import SwiftUI
+import SwiftData
+import Charts
 
 struct WeightChartView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var weights: [Weight]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart {
+            ForEach(weights) { weight in
+                BarMark(x: .value("Date", weight.date),
+                        y: .value("Weight in grams", weight.weight))
+                .annotation(position: .top) {
+                    Text("\(String(weight.weight))g")
+                        .foregroundStyle(.secondary)
+                        .font(.system(size: 10.0))
+                        .fontWeight(.semibold)
+                }
+            }
+        }
+        .chartScrollableAxes(.horizontal)
+        .chartXVisibleDomain(length: 6)
+        .chartLegend(alignment: .top, spacing: 10){
+            Text("Weight history")
+                .font(.headline)
+                .foregroundStyle(.secondary)
+        }
+        .scaledToFit()
+        .chartYAxisLabel {
+            Text("Weight in grams")
+        }
+        .chartXAxisLabel(alignment: .center) {
+            Text("Weights from \(weights.first?.date.formatted(.dateTime) ?? "")-\(weights.last?.date.formatted(.dateTime) ?? "")")
+        }
+        .chartXAxis {
+            AxisMarks {
+                AxisValueLabel {}
+            }
+        }
     }
 }
 
