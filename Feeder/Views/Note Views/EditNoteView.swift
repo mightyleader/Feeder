93829ea5dca1327c45eb50aca0f5d82bf0233009
Rob Stearn <robstearn@me.com>
2024-11-12 17:34:17 +0400

    Undid change

---
+++ Feeder/Views/Note Views/EditNoteView.swift
@@ -0,0 +1,18 @@
+//
+//  EditNoteView.swift
+//  Feeder
+//
+//  Created by Rob Stearn on 12/11/2024.
+//
+
+import SwiftUI
+
+struct EditNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditNoteView()
+}
