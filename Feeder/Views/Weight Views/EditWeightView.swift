122c9bf78d70fb1e93749b75a7f88ccfc46e63f0
Rob Stearn <robstearn@me.com>
2024-11-12 16:47:24 +0400

    Added view for editing weight entries

--- Feeder/Views/Weight Views/EditWeightView.swift
+++ Feeder/Views/Weight Views/EditWeightView.swift
@@ -8,11 +8,75 @@
 import SwiftUI
 
 struct EditWeightView: View {
+    @Environment(\.modelContext) private var modelContext
+    @State var weight: Weight
+    
+    private let numberFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
+    }()
+    
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            DatePicker(
+                    "Weighing Date",
+                    selection: $weight.date,
+                    displayedComponents: [.date, .hourAndMinute]
+                )
+            .datePickerStyle(.graphical)
+            .tint(.green)
+            
+            HStack {
+                Spacer()
+                TextField("Weight", value: $weight.weight, formatter: numberFormatter)
+                    .keyboardType(.numberPad)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .tint(.green)
+                Text(" grams")
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+            }
+            
+            HStack {
+                Spacer()
+                Picker("Type", selection: $weight.type) {
+                    ForEach(WeightType.allCases) { type in
+                        Text(type.rawValue)
+                    }
+                }
+                .pickerStyle(InlinePickerStyle())
+                .tint(.green)
+                .font(.headline)
+                .fontWeight(.heavy)
+                Spacer()
+            }
+            
+            Button("Edit Weight") {
+                self.save()
+            }
+            .font(.headline)
+            .fontWeight(.heavy)
+            .buttonStyle(.borderedProminent)
+            .foregroundStyle(.white)
+            .tint(.green)
+            Spacer()
+        }
+    }
+    
+    private func save() {
+        try? self.modelContext.save()
+        self.dismiss()
     }
 }
 
 #Preview {
-    EditWeightView()
+    EditWeightView(weight: Weight(weight: 3000.00,
+                                  type: .weight,
+                                  date: Date()))
 }
