4b76965e2d1d29ed59de42bf1e944ae49463c4c4
Rob Stearn <robstearn@me.com>
2024-11-12 16:48:03 +0400

    changed layout of totals view

--- Feeder/Views/Feed Views/TotalView.swift
+++ Feeder/Views/Feed Views/TotalView.swift
@@ -15,6 +15,12 @@ struct TotalView: View {
     
     var body: some View {
         VStack {
+            if highlightTarget == true {
+                Text("Daily target is \(Int(feedTarget ?? 500)) ml")
+                    .font(.footnote)
+                    .tint(.gray)
+                    .fontWeight(.semibold)
+            }
             HStack {
                 Text("🍼")
                     .font(.title)
@@ -29,12 +35,6 @@ struct TotalView: View {
                     .fontWeight(.heavy)
                     .foregroundColor(highlightTarget ? ( qty_ml >= /*self.dailyFeedTarget*/ Int(feedTarget ?? 500) ? .green : .red) : .primary)
                     .padding()
-            }
-            if highlightTarget == true {
-                Text("Daily target is \(Int(feedTarget ?? 500)) ml")
-                    .font(.footnote)
-                    .tint(.gray)
-                    .fontWeight(.semibold)
             }
         }
     }
