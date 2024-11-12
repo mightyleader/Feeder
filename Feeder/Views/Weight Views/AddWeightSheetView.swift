7073f69c920a17798eb15037cc8095f37652e4c9
Rob Stearn <robstearn@me.com>
2024-11-12 16:47:46 +0400

    Added view for adding weight entries

--- Feeder/Views/Weight Views/AddWeightSheetView.swift
+++ Feeder/Views/Weight Views/AddWeightSheetView.swift
@@ -6,13 +6,85 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddWeightSheetView: View {
+    @Environment(\.modelContext) private var modelContext
+    @State var weight: String = "0.0"
+    @State var type: WeightType = .weight
+    @State var date: Date = Date()
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+                Button {
+                    self.dismiss()
+                } label: {
+                    Label("", systemImage: "xmark")
+                }
+            }
+            .padding()
+            
+            DatePicker(
+                    "Weighing Date",
+                    selection: $date,
+                    displayedComponents: [.date, .hourAndMinute]
+                )
+            .datePickerStyle(.graphical)
+            .tint(.green)
+            
+            HStack {
+                Spacer()
+                TextField("Weight", text: $weight)
+                    .keyboardType(.numberPad)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .tint(.green)
+                Text(" grams")
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+            }
+            
+            HStack {
+                Spacer()
+                Picker("Type", selection: $type) {
+                    ForEach(WeightType.allCases) { type in
+                        Text(type.rawValue)
+                    }
+                }
+                .pickerStyle(InlinePickerStyle())
+                .tint(.green)
+                .font(.headline)
+                .fontWeight(.heavy)
+                Spacer()
+            }
+            
+            Button("Add Weight") {
+                self.addItem()
+            }
+            .font(.headline)
+            .fontWeight(.heavy)
+            .buttonStyle(.borderedProminent)
+            .foregroundStyle(.white)
+            .tint(.green)
+            Spacer()
+        }
+    }
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Weight(weight: Double(weight) ?? 0.0, type: type, date: date)
+            modelContext.insert(newItem)
+            self.dismiss()
+        }
     }
 }
 
 #Preview {
-    AddWeightSheetView()
+    AddWeightSheetView(type: .birthWeight)
 }
