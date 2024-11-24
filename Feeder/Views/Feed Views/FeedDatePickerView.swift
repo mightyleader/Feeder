97e3676bdc1e4e7237db068c0ac5c32ffb616d6f
Rob Stearn <robstearn@me.com>
2024-11-24 12:47:10 +0400

    Created a view for selecting a specific date

--- Feeder/Views/Feed Views/FeedDatePickerView.swift
+++ Feeder/Views/Feed Views/FeedDatePickerView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct FeedDatePickerView: View {
+    @Environment(\.dismiss) var dismiss
+    @Binding var date: Date
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button {
+                    self.dismiss()
+                } label: {
+                    Label("Cancel", image: "xmark")
+                }
+                Spacer()
+                Button {
+                    self.date = .now
+                } label: {
+                    Label("Today", image: "pin.circle.fill")
+                }
+            }
+            .padding()
+        }
+        DatePicker(
+                "Feed Date",
+                selection: $date,
+                displayedComponents: [.date]
+            )
+        .datePickerStyle(.graphical)
+        .tint(.green)
+        .onChange(of: date, initial: false) { oldValue, newValue in
+            self.dismiss()
+        }
     }
 }
 
-#Preview {
-    FeedDatePickerView()
-}
+//#Preview {
+//    FeedDatePickerView()
+//}
