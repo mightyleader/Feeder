7985944334fb24e356ce29a1890186103804ab20
Rob Stearn <robstearn@me.com>
2024-11-24 12:47:56 +0400

    Added a property to store the filter mode and added it throught the struct

--- Feeder/Views/Feed Views/FeedsView.swift
+++ Feeder/Views/Feed Views/FeedsView.swift
@@ -11,20 +11,24 @@ import SwiftData
 struct FeedsView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var feeds: [Feed]
+    @State private var selectedDate: Date
+    @State private var showDatePicker: Bool = false
     
     private var todayFormat: Date.FormatStyle = Date.FormatStyle(date: .none,
                                                                  time: .standard)
-    private var historyFormat: Date.FormatStyle = Date.FormatStyle(date: .complete,
+    private var historyFormat: Date.FormatStyle = Date.FormatStyle(date: .abbreviated,
                                                                    time: .shortened)
     
     private var showTodayOnly: Bool
+    private var filterMode: FeederDateFilter
                             
-
-    init(limitingDate: Date, showTodayOnly: Bool) {
+    init(limitingDate: Date, showTodayOnly: Bool, filterMode: FeederDateFilter) {
+        self.filterMode = filterMode
         self.showTodayOnly = showTodayOnly
         _feeds = Query(filter: #Predicate<Feed> { feed in
             feed.timestamp >= limitingDate
         }, sort: \Feed.timestamp)
+        self.selectedDate = .now
     }
     
     var body: some View {
@@ -35,7 +39,7 @@ struct FeedsView: View {
                         EditFeedSheetView(feed: feed)
                     } label: {
                         HStack {
-                            Text("\(feed.timestamp, format: showTodayOnly ? todayFormat : historyFormat)").foregroundStyle(.gray)
+                            Text("\(feed.timestamp, format: historyFormat)").foregroundStyle(.gray)
                             Spacer()
                             SourceLabel(source: feed.source)
                             FeedLabel(qty: feed.qty_as_int)
@@ -44,7 +48,11 @@ struct FeedsView: View {
                 }
                 .onDelete(perform: deleteItems)
             }
-            TotalView(qty_ml: total(items: self.feeds), highlightTarget: showTodayOnly)
+            TotalView(qty_ml: total(items: self.feeds), highlightTarget: showTodayOnly, filterMode: self.filterMode)
+        }
+        .sheet(isPresented: $showDatePicker) {
+            FeedDatePickerView(date: $selectedDate)
+                .presentationDetents([.medium])
         }
     }
     
@@ -96,5 +104,5 @@ extension FeedsView {
 }
 
 #Preview {
-    FeedsView(limitingDate: Date.distantPast, showTodayOnly: true)
+    FeedsView(limitingDate: Date.distantPast, showTodayOnly: true, filterMode: .today)
 }
