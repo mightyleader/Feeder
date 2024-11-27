4e60a3d02f66c79e126d4cacac00b608e7f1c839
Rob Stearn <robstearn@me.com>
2024-11-27 22:42:06 +0400

    Added specific dates

--- Feeder/Views/Feed Views/FeedFilterModeView.swift
+++ Feeder/Views/Feed Views/FeedFilterModeView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct FeedFilterModeView: View {
     @Binding var filterMode: FeederDateFilter
+    @Binding var limitingDateRange: ClosedRange<Date>
+    var dateFormat: Date.FormatStyle = Date.FormatStyle(date: .abbreviated)
     var body: some View {
         HStack {
-            Text(self.textFrom(filterMode: self.filterMode))
+            Text(self.textFrom(filterMode: self.filterMode,
+                               forDateRange: self.limitingDateRange))
                 .font(.caption2)
                 .fontWeight(.semibold)
                 .foregroundStyle(.gray)
         }
     }
     
-    private func textFrom(filterMode: FeederDateFilter) -> String {
+    private func textFrom(filterMode: FeederDateFilter,
+                          forDateRange dateRange: ClosedRange<Date>) -> String {
         switch filterMode {
         case .today:
             return "Showing feeds for today"
@@ -29,9 +33,9 @@ struct FeedFilterModeView: View {
         case .last30days:
             return "Showing feeds for the last 30 days"
         case .singleDate:
-            return "Showing feeds for a single date"
+            return "Showing feeds for \(dateRange.lowerBound.formatted(dateFormat))"
         case .dateRange:
-            return "Showing feeds for a date range"
+            return "Showing feeds for dates between \(dateRange.lowerBound.formatted(dateFormat)) and \(dateRange.upperBound.formatted(dateFormat))"
         }
     }
 }
