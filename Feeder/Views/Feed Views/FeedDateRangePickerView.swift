d7bba42eacbe454424e3ca8c661b3519fdf5e26d
Rob Stearn <robstearn@me.com>
2024-11-27 04:47:10 +0400

    Complerted range picker

--- Feeder/Views/Feed Views/FeedDateRangePickerView.swift
+++ Feeder/Views/Feed Views/FeedDateRangePickerView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct FeedDateRangePickerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.calendar) var calendar
+    @State var fromDate: Date = Calendar.autoupdatingCurrent.startOfDay(for: .now).advanced(by: 86400 * -10)
+    @State var toDate: Date = .now
+    @Binding var dateQueryRange: ClosedRange<Date>
 
-#Preview {
-    FeedDateRangePickerView()
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    self.toDate = .now
+                } label: {
+                    Label("Today", systemImage: "pin.circle.fill")
+                }
+                Spacer()
+                Button {
+                    self.dismiss()
+                } label: {
+                    Label("", systemImage: "xmark")
+                }
+            }
+            DatePicker("From",
+                       selection: $fromDate,
+                       displayedComponents: [.date])
+            .datePickerStyle(.automatic)
+            DatePicker("From",
+                       selection: $toDate,
+                       displayedComponents: [.date])
+            .datePickerStyle(.automatic)
+            Button("Choose dates") {
+                self.dateQueryRange = self.fromDate...calendar.startOfDay(for: self.toDate).advanced(by: 86399)
+                self.dismiss()
+            }
+            .font(.headline)
+            .fontWeight(.heavy)
+            .buttonStyle(.borderedProminent)
+            .foregroundStyle(.white)
+            .tint(.green)
+            Spacer()
+        }
+        .padding()
+        .tint(.green)
+    }
 }
