e2b712747c855d2f7d1fc657d2c3b130007197bf
Rob Stearn <robstearn@me.com>
2024-11-27 22:20:11 +0400

    New view to show what the current filter is showing.

--- Feeder/Views/Feed Views/FeedFilterModeView.swift
+++ Feeder/Views/Feed Views/FeedFilterModeView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct FeedFilterModeView: View {
+    @Binding var filterMode: FeederDateFilter
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(self.textFrom(filterMode: self.filterMode))
+                .font(.caption2)
+                .fontWeight(.semibold)
+                .foregroundStyle(.gray)
+        }
     }
-}
-
-#Preview {
-    FeedFilterModeView()
+    
+    private func textFrom(filterMode: FeederDateFilter) -> String {
+        switch filterMode {
+        case .today:
+            return "Showing feeds for today"
+        case .allTime:
+            return "Showing all feeds"
+        case .last7days:
+            return "Showing feeds for the last 7 days"
+        case .last30days:
+            return "Showing feeds for the last 30 days"
+        case .singleDate:
+            return "Showing feeds for a single date"
+        case .dateRange:
+            return "Showing feeds for a date range"
+        }
+    }
 }
